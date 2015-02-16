1de1d11142d9c4f59d5f49e32325070d88ca4510
Julian Fisch <julian.fisch@asinfotrack.ch>
2015-02-16 10:51:29 +0100

    added RoutePlannerConsoleApp

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RoutePlanner (Version " + Assembly.GetExecutingAssembly().GetName().Version + ")");
+            Console.ReadLine();
+        }
+    }
+}
