36f670a95b452db64822f232dc1fd182d10a6e3a
timonkrebs <timon.krebs@stundets.fhnw.ch>
2015-02-16 12:21:15 +0100

    Test revert

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -12,7 +12,6 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RoutePlanner (Version " + Assembly.GetExecutingAssembly().GetName().Version + ")");
-            Console.WriteLine("Welcome to RoutePlanner is now changed)");
             Console.ReadLine();
         }
     }
