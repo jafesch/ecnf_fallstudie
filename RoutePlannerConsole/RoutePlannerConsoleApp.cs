4922d66153e67e9337dd3c89da92c22dbdd2dc24
timonkrebs <timon.krebs@stundets.fhnw.ch>
2015-02-16 20:31:39 +0100

    added WayPoint.cs

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -12,6 +13,8 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RoutePlanner (Version " + Assembly.GetExecutingAssembly().GetName().Version + ")");
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude); 
             Console.ReadLine();
         }
     }
